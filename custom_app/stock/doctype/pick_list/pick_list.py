3a6d4342975be21188b0f11ad7857e5503519ddb
1337it <91686534+1337it@users.noreply.github.com>
2024-10-19 11:50:07 +0400

    Update pick_list.py

--- custom_app/stock/doctype/pick_list/pick_list.py
+++ custom_app/stock/doctype/pick_list/pick_list.py
@@ -805,6 +805,7 @@ def get_available_item_locations(
 	item_code,
 	from_warehouses,
 	required_qty,
+	company,
 	ignore_validation=False,
 	picked_item_details=None,
 	consider_rejected_warehouses=False,
@@ -837,6 +838,7 @@ def get_available_item_locations(
 		locations = get_available_item_locations_for_other_item(
 			item_code,
 			from_warehouses,
+			company,
 			consider_rejected_warehouses=consider_rejected_warehouses,
 		)
 
