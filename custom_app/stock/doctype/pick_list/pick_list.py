4ec777757d237749ebd93dd8a7683e7d645dd606
1337it <91686534+1337it@users.noreply.github.com>
2024-10-19 14:41:59 +0400

    Update pick_list.py

--- custom_app/stock/doctype/pick_list/pick_list.py
+++ custom_app/stock/doctype/pick_list/pick_list.py
@@ -1067,7 +1067,7 @@ def get_available_item_locations_for_other_item(
 		.select(bin.warehouse, (
 			frappe.qb.from_(bin2)
 			.select(bin2.actualy_qty)
-			.where(bin2.warehouse == bin.warehouse) and (bin2.actualy_qty > 0) and (bin2.item_code == item_code)).as_("qty"))
+			.where(bin2.item_code == item_code) and (bin2.warehouse == bin.warehouse) and (bin2.actualy_qty > 0)).as_("qty"))
 		.where(bin.company == company)
 		.orderby(bin.rank)
 	)
