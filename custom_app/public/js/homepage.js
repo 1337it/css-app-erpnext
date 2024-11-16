c447cb5254c3362203668059ffa9577bdd6ba9bd
1337it <91686534+1337it@users.noreply.github.com>
2024-11-16 12:10:22 +0400

    Update homepage.js

--- custom_app/public/js/homepage.js
+++ custom_app/public/js/homepage.js
@@ -1344,7 +1344,7 @@ document.querySelector('[data-page-route^="'+frappe.get_route()[1]+'"] .page-hea
 frappe.ui.form.on(doctype, "refresh", function(frm) {
 	var formlinks = document.querySelector('[data-page-route="'+doctype+'"] .form-links') !== null;
 			if(formlinks){
-  $('[data-page-route="'+doctype+'"] .col-lg-2.layout-side-section').append($('[data-page-route="'+doctype+'"] .form-links')[0].clone());
+  $('[data-page-route="'+doctype+'"] .col-lg-2.layout-side-section').append($('[data-page-route="'+doctype+'"] .form-links').clone()[0]);
 			}
 	var path = '[href="'+window.location.pathname+'"].ellipsis';
 $(".list-row-container").removeClass("selecteditem");
